6599058429f499dee705fbc926c6eae707de503d
Stephan Nordnes Eriksen <stephanruler@gmail.com>
2015-04-03 14:01:57 +0200

    Adding .gitignore and fixing up in bower.json. Compiling new js

--- public/BOB.js
+++ public/BOB.js
@@ -1,1 +1,503 @@
-!function(){"use strict";var t="undefined"!=typeof window?window:global;if("function"!=typeof t.require){var i={},r={},o=function(t,i){return{}.hasOwnProperty.call(t,i)},n=function(t,i){var r,o,n=[];r=/^\.\.?(\/|$)/.test(i)?[t,i].join("/").split("/"):i.split("/");for(var s=0,e=r.length;e>s;s++)o=r[s],".."===o?n.pop():"."!==o&&""!==o&&n.push(o);return n.join("/")},s=function(t){return t.split("/").slice(0,-1).join("/")},e=function(i){return function(r){var o=s(i),e=n(o,r);return t.require(e,i)}},h=function(t,i){var o={id:t,exports:{}};return r[t]=o,i(o.exports,e(t),o),o.exports},u=function(t,s){var e=n(t,".");if(null==s&&(s="/"),o(r,e))return r[e].exports;if(o(i,e))return h(e,i[e]);var u=n(e,"./index");if(o(r,u))return r[u].exports;if(o(i,u))return h(u,i[u]);throw new Error('Cannot find module "'+t+'" from "'+s+'"')},a=function(t,r){if("object"==typeof t)for(var n in t)o(t,n)&&(i[n]=t[n]);else i[t]=r},p=function(){var t=[];for(var r in i)o(i,r)&&t.push(r);return t};t.require=u,t.require.define=a,t.require.register=a,t.require.list=p,t.require.brunch=!0}}(),require.register("scripts/BOB",function(t,i,r){var o;o=function(){function t(i,r,o,n,s,e){var h,u,a,p;if(null==o&&(o=null),null==n&&(n=null),null==s&&(s=null),null==e&&(e=null),i.indexOf(" ")>-1)return console.error('Invalid BOB selector. "'+i+'" contains " "(space). Only allowed is "tag", "tag.class", or "tag#id".'),"Invalid selector. See console log for more details.";if(this.parent=o,this.options={},r)for(h in r)u=r[h],this.options[h]=t.toVariable(u);this.preBob=n,this.innerBob=s,this.postBob=e,this.type=i,this.object_class=null,this.object_id=null,this.object_content="",this.object_style=null,i.indexOf(".")>-1?(a=i.split("."),this.type=a[0],this.object_class=a[1]):i.indexOf("#")>-1&&(p=i.split("#"),this.type=p[0],this.object_id=p[1])}return t._data=null,t.find=function(t){},t.data=function(){return t._data},t.d=function(){return t._data},t.get_or_create_bob=function(i,r,o){var n;return n=null,n=i instanceof t?i:new t(i,r,o)},t.toVariable=function(t){return"function"==typeof t?t():t},t.prototype.content=function(t){return this.co(t)},t.prototype.co=function(i){return this.object_content=t.toVariable(i),this},t.prototype.style=function(t){return this.st(t)},t.prototype.st=function(i){return this.object_style=t.toVariable(i),this},t.prototype["class"]=function(t){return this.cl(t)},t.prototype.cl=function(i){return this.object_class=t.toVariable(i),this},t.prototype.id=function(i){return this.object_id=t.toVariable(i),this},t.prototype.insert=function(t,i){return this.i(t,i)},t.prototype.i=function(i,r){var o;return o=t.get_or_create_bob(i,r,this),this.innerBob?this.innerBob.a(o):this.innerBob=o,o},t.prototype.append=function(t,i){return this.a(t,i)},t.prototype.a=function(i,r){var o;return o=t.get_or_create_bob(i,r,this),this.postBob?this.postBob.a(o):this.postBob=o},t.prototype.prepend=function(t,i){return this.p(t,i)},t.prototype.p=function(i,r){var o;return o=t.get_or_create_bob(i,r,this),this.preBob?this.preBob.p(o):this.preBob=o},t.prototype["do"]=function(t){return this.d(t)},t.prototype.d=function(t){var i;return i=new BOBChildArray(t,this)},t.prototype.up=function(){return this.parent},t.prototype.u=function(){return this.parent},t.prototype.toString=function(){return this.s()},t.prototype.s=function(){var t,i,r,o,n,s,e;if(this.parent)return this.parent.s();this.innerBob&&(this.innerBob.parent=null),this.preBob&&(this.preBob.parent=null),this.postBob&&(this.postBob.parent=null),o="",t="",n="",i="",this.innerBob&&(i=this.innerBob.s()),this.preBob&&(o=this.preBob.s()),this.postBob&&(t=this.postBob.s()),n+="<"+this.type+" ",e=this.options;for(r in e)s=e[r],"style"===r&&this.object_style||"id"===r&&this.object_id||"class"===r&&this.object_class||(n+=r+'="'+s+'" ');return this.object_class&&(n+='class="'+this.object_class+'" '),this.object_id&&(n+='id="'+this.object_id+'" '),this.object_style&&(n+='style="'+this.object_style+'" '),n=n.slice(0,-1),n+=">"+this.object_content+i+"</"+this.type+">",o+n+t},t}()}),require.register("scripts/BOBChildArray",function(t,r,o){var n;n=function(){function t(t,i){this.dataset=t,this.parent=i,this.bobs=[]}return t.prototype.content=function(t){return this.co(t)},t.prototype.co=function(t){var i,r,o;for(i=r=0,o=this.dataset.length;o>=0?o>r:r>o;i=o>=0?++r:--r)BOB._data=this.dataset[i],this.bobs[i]&&this.bobs[i].co(t);return this},t.prototype.style=function(t){return this.st(t)},t.prototype.st=function(t){var i,r,o;for(i=r=0,o=this.dataset.length;o>=0?o>r:r>o;i=o>=0?++r:--r)BOB._data=this.dataset[i],this.bobs[i]&&this.bobs[i].st(t);return this},t.prototype["class"]=function(t){return this.cl(t)},t.prototype.cl=function(t){var i,r,o;for(i=r=0,o=this.dataset.length;o>=0?o>r:r>o;i=o>=0?++r:--r)BOB._data=this.dataset[i],this.bobs[i]&&this.bobs[i].cl(t);return this},t.prototype.id=function(t){var i,r,o;for(i=r=0,o=this.dataset.length;o>=0?o>r:r>o;i=o>=0?++r:--r)BOB._data=this.dataset[i],this.bobs[i]&&this.bobs[i].id(t);return this},t.prototype.insert=function(t,i){return this.i(t,i)},t.prototype.i=function(t,i){var r,o,n;for(r=o=0,n=this.dataset.length;n>=0?n>o:o>n;r=n>=0?++o:--o)BOB._data=this.dataset[r],this.bobs[r]?this.bobs[r]=this.bobs[r].insert(t,i):this.bobs.push(this.parent.insert(t,i));return this},t.prototype.append=function(t,i){return this.a(t,i)},t.prototype.a=function(t,i){var r,o,n;for(r=o=0,n=this.dataset.length;n>=0?n>o:o>n;r=n>=0?++o:--o)BOB._data=this.dataset[r],this.bobs[r]?this.bobs[r]=this.bobs[r].a(t,i):this.bobs.push(this.parent.a(t,i));return this},t.prototype.prepend=function(t,i){return this.p(t,i)},t.prototype.p=function(t,i){var r,o,n;for(r=o=0,n=this.dataset.length;n>=0?n>o:o>n;r=n>=0?++o:--o)BOB._data=this.dataset[r],this.bobs[r]?this.bobs[r]=this.bobs[r].p(t,i):this.bobs.push(this.parent.p(t,i));return this},t.prototype.toString=function(){return this.s()},t.prototype.s=function(){var t,i,r,o,n;if(this.parent)return this.parent.s();for(i="",n=this.bobs,r=0,o=n.length;o>r;r++)t=n[r],t.parent=!1,i+=t.s();return i},t.prototype["do"]=function(t){return this.d(t)},t.prototype.d=function(t){var r,o,n,s;for(s=this.bobs,o=0,n=s.length;n>o;o++)r=s[o],BOB._data=this.dataset[i],this.bobs[i]?this.bobs[i]=this.bobs[i].d(t):this.bobs.push(this.parent.d(t));return this},t.prototype.up=function(){return this.u()},t.prototype.u=function(){var t,i,r;if(!this.bobs[0])return BOB._data=null,this.parent;for(t=i=0,r=this.bobs.length;r>=0?r>i:i>r;t=r>=0?++i:--i)this.bobs[t]=this.bobs[t].u();return this.bobs[0]===this.parent?(BOB._data=null,this.parent):this},t}()});
+(function(/*! Brunch !*/) {
+  'use strict';
+
+  var globals = typeof window !== 'undefined' ? window : global;
+  if (typeof globals.require === 'function') return;
+
+  var modules = {};
+  var cache = {};
+
+  var has = function(object, name) {
+    return ({}).hasOwnProperty.call(object, name);
+  };
+
+  var expand = function(root, name) {
+    var results = [], parts, part;
+    if (/^\.\.?(\/|$)/.test(name)) {
+      parts = [root, name].join('/').split('/');
+    } else {
+      parts = name.split('/');
+    }
+    for (var i = 0, length = parts.length; i < length; i++) {
+      part = parts[i];
+      if (part === '..') {
+        results.pop();
+      } else if (part !== '.' && part !== '') {
+        results.push(part);
+      }
+    }
+    return results.join('/');
+  };
+
+  var dirname = function(path) {
+    return path.split('/').slice(0, -1).join('/');
+  };
+
+  var localRequire = function(path) {
+    return function(name) {
+      var dir = dirname(path);
+      var absolute = expand(dir, name);
+      return globals.require(absolute, path);
+    };
+  };
+
+  var initModule = function(name, definition) {
+    var module = {id: name, exports: {}};
+    cache[name] = module;
+    definition(module.exports, localRequire(name), module);
+    return module.exports;
+  };
+
+  var require = function(name, loaderPath) {
+    var path = expand(name, '.');
+    if (loaderPath == null) loaderPath = '/';
+
+    if (has(cache, path)) return cache[path].exports;
+    if (has(modules, path)) return initModule(path, modules[path]);
+
+    var dirIndex = expand(path, './index');
+    if (has(cache, dirIndex)) return cache[dirIndex].exports;
+    if (has(modules, dirIndex)) return initModule(dirIndex, modules[dirIndex]);
+
+    throw new Error('Cannot find module "' + name + '" from '+ '"' + loaderPath + '"');
+  };
+
+  var define = function(bundle, fn) {
+    if (typeof bundle === 'object') {
+      for (var key in bundle) {
+        if (has(bundle, key)) {
+          modules[key] = bundle[key];
+        }
+      }
+    } else {
+      modules[bundle] = fn;
+    }
+  };
+
+  var list = function() {
+    var result = [];
+    for (var item in modules) {
+      if (has(modules, item)) {
+        result.push(item);
+      }
+    }
+    return result;
+  };
+
+  globals.require = require;
+  globals.require.define = define;
+  globals.require.register = define;
+  globals.require.list = list;
+  globals.require.brunch = true;
+})();
+require.register("scripts/BOB", function(exports, require, module) {
+var BOB;
+
+BOB = (function() {
+  BOB._data = null;
+
+  BOB.find = function(selector) {};
+
+  BOB.data = function() {
+    return BOB._data;
+  };
+
+  BOB.d = function() {
+    return BOB._data;
+  };
+
+  BOB.get_or_create_bob = function(data, options, parent) {
+    var child_bob;
+    child_bob = null;
+    if (data instanceof BOB) {
+      child_bob = data;
+    } else {
+      child_bob = new BOB(data, options, parent);
+    }
+    return child_bob;
+  };
+
+  BOB.toVariable = function(data) {
+    if (typeof data === 'function') {
+      return data();
+    } else {
+      return data;
+    }
+  };
+
+  function BOB(selector, options, parent, preBob, contentBob, postBob) {
+    var key, value, _ref, _ref1;
+    if (parent == null) {
+      parent = null;
+    }
+    if (preBob == null) {
+      preBob = null;
+    }
+    if (contentBob == null) {
+      contentBob = null;
+    }
+    if (postBob == null) {
+      postBob = null;
+    }
+    if (selector.indexOf(" ") > -1) {
+      console.error("Invalid BOB selector. \"" + selector + "\" contains \" \"(space). Only allowed is \"tag\", \"tag.class\", or \"tag#id\".");
+      return "Invalid selector. See console log for more details.";
+    }
+    this.parent = parent;
+    this.options = {};
+    if (options) {
+      for (key in options) {
+        value = options[key];
+        this.options[key] = BOB.toVariable(value);
+      }
+    }
+    this.preBob = preBob;
+    this.innerBob = contentBob;
+    this.postBob = postBob;
+    this.type = selector;
+    this.object_class = null;
+    this.object_id = null;
+    this.object_content = "";
+    this.object_style = null;
+    if (selector.indexOf(".") > -1) {
+      _ref = selector.split("."), this.type = _ref[0], this.object_class = _ref[1];
+    } else if (selector.indexOf("#") > -1) {
+      _ref1 = selector.split("#"), this.type = _ref1[0], this.object_id = _ref1[1];
+    }
+  }
+
+  BOB.prototype.content = function(content) {
+    return this.co(content);
+  };
+
+  BOB.prototype.co = function(content) {
+    this.object_content = BOB.toVariable(content);
+    return this;
+  };
+
+  BOB.prototype.style = function(style) {
+    return this.st(style);
+  };
+
+  BOB.prototype.st = function(style) {
+    this.object_style = BOB.toVariable(style);
+    return this;
+  };
+
+  BOB.prototype["class"] = function(object_class) {
+    return this.cl(object_class);
+  };
+
+  BOB.prototype.cl = function(object_class) {
+    this.object_class = BOB.toVariable(object_class);
+    return this;
+  };
+
+  BOB.prototype.id = function(object_id) {
+    this.object_id = BOB.toVariable(object_id);
+    return this;
+  };
+
+  BOB.prototype.insert = function(data, options) {
+    return this.i(data, options);
+  };
+
+  BOB.prototype.i = function(data, options) {
+    var child_bob;
+    child_bob = BOB.get_or_create_bob(data, options, this);
+    if (this.innerBob) {
+      this.innerBob.a(child_bob);
+    } else {
+      this.innerBob = child_bob;
+    }
+    return child_bob;
+  };
+
+  BOB.prototype.append = function(data, options) {
+    return this.a(data, options);
+  };
+
+  BOB.prototype.a = function(data, options) {
+    var new_bob;
+    new_bob = BOB.get_or_create_bob(data, options, this);
+    if (this.postBob) {
+      return this.postBob.a(new_bob);
+    } else {
+      return this.postBob = new_bob;
+    }
+  };
+
+  BOB.prototype.prepend = function(data, options) {
+    return this.p(data, options);
+  };
+
+  BOB.prototype.p = function(data, options) {
+    var new_bob;
+    new_bob = BOB.get_or_create_bob(data, options, this);
+    if (this.preBob) {
+      return this.preBob.p(new_bob);
+    } else {
+      return this.preBob = new_bob;
+    }
+  };
+
+  BOB.prototype["do"] = function(dataset) {
+    return this.d(dataset);
+  };
+
+  BOB.prototype.d = function(dataset) {
+    var child_array;
+    child_array = new BOBChildArray(dataset, this);
+    return child_array;
+  };
+
+  BOB.prototype.up = function() {
+    return this.parent;
+  };
+
+  BOB.prototype.u = function() {
+    return this.parent;
+  };
+
+  BOB.prototype.toString = function() {
+    return this.s();
+  };
+
+  BOB.prototype.s = function() {
+    var append, content_b, key, prepend, printself, value, _ref;
+    if (this.parent) {
+      return this.parent.s();
+    }
+    if (this.innerBob) {
+      this.innerBob.parent = null;
+    }
+    if (this.preBob) {
+      this.preBob.parent = null;
+    }
+    if (this.postBob) {
+      this.postBob.parent = null;
+    }
+    prepend = '';
+    append = '';
+    printself = '';
+    content_b = '';
+    if (this.innerBob) {
+      content_b = this.innerBob.s();
+    }
+    if (this.preBob) {
+      prepend = this.preBob.s();
+    }
+    if (this.postBob) {
+      append = this.postBob.s();
+    }
+    printself += '<' + this.type + ' ';
+    _ref = this.options;
+    for (key in _ref) {
+      value = _ref[key];
+      if (!(key === 'style' && this.object_style || key === 'id' && this.object_id || key === 'class' && this.object_class)) {
+        printself += key + '="' + value + '" ';
+      }
+    }
+    if (this.object_class) {
+      printself += 'class="' + this.object_class + '" ';
+    }
+    if (this.object_id) {
+      printself += 'id="' + this.object_id + '" ';
+    }
+    if (this.object_style) {
+      printself += 'style="' + this.object_style + '" ';
+    }
+    printself = printself.slice(0, -1);
+    printself += '>' + this.object_content + content_b + '</' + this.type + '>';
+    return prepend + printself + append;
+  };
+
+  return BOB;
+
+})();
+});
+
+require.register("scripts/BOBChildArray", function(exports, require, module) {
+var BOBChildArray;
+
+BOBChildArray = (function() {
+  function BOBChildArray(dataset, parent) {
+    this.dataset = dataset;
+    this.parent = parent;
+    this.bobs = [];
+  }
+
+  BOBChildArray.prototype.content = function(content) {
+    return this.co(content);
+  };
+
+  BOBChildArray.prototype.co = function(content) {
+    var i, _i, _ref;
+    for (i = _i = 0, _ref = this.dataset.length; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
+      BOB._data = this.dataset[i];
+      if (this.bobs[i]) {
+        this.bobs[i].co(content);
+      }
+    }
+    return this;
+  };
+
+  BOBChildArray.prototype.style = function(style) {
+    return this.st(style);
+  };
+
+  BOBChildArray.prototype.st = function(style) {
+    var i, _i, _ref;
+    for (i = _i = 0, _ref = this.dataset.length; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
+      BOB._data = this.dataset[i];
+      if (this.bobs[i]) {
+        this.bobs[i].st(style);
+      }
+    }
+    return this;
+  };
+
+  BOBChildArray.prototype["class"] = function(object_class) {
+    return this.cl(object_class);
+  };
+
+  BOBChildArray.prototype.cl = function(object_class) {
+    var i, _i, _ref;
+    for (i = _i = 0, _ref = this.dataset.length; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
+      BOB._data = this.dataset[i];
+      if (this.bobs[i]) {
+        this.bobs[i].cl(object_class);
+      }
+    }
+    return this;
+  };
+
+  BOBChildArray.prototype.id = function(object_id) {
+    var i, _i, _ref;
+    for (i = _i = 0, _ref = this.dataset.length; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
+      BOB._data = this.dataset[i];
+      if (this.bobs[i]) {
+        this.bobs[i].id(object_id);
+      }
+    }
+    return this;
+  };
+
+  BOBChildArray.prototype.insert = function(data, options) {
+    return this.i(data, options);
+  };
+
+  BOBChildArray.prototype.i = function(data, options) {
+    var i, _i, _ref;
+    for (i = _i = 0, _ref = this.dataset.length; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
+      BOB._data = this.dataset[i];
+      if (this.bobs[i]) {
+        this.bobs[i] = this.bobs[i].insert(data, options);
+      } else {
+        this.bobs.push(this.parent.insert(data, options));
+      }
+    }
+    return this;
+  };
+
+  BOBChildArray.prototype.append = function(data, options) {
+    return this.a(data, options);
+  };
+
+  BOBChildArray.prototype.a = function(data, options) {
+    var i, _i, _ref;
+    for (i = _i = 0, _ref = this.dataset.length; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
+      BOB._data = this.dataset[i];
+      if (this.bobs[i]) {
+        this.bobs[i] = this.bobs[i].a(data, options);
+      } else {
+        this.bobs.push(this.parent.a(data, options));
+      }
+    }
+    return this;
+  };
+
+  BOBChildArray.prototype.prepend = function(data, options) {
+    return this.p(data, options);
+  };
+
+  BOBChildArray.prototype.p = function(data, options) {
+    var i, _i, _ref;
+    for (i = _i = 0, _ref = this.dataset.length; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
+      BOB._data = this.dataset[i];
+      if (this.bobs[i]) {
+        this.bobs[i] = this.bobs[i].p(data, options);
+      } else {
+        this.bobs.push(this.parent.p(data, options));
+      }
+    }
+    return this;
+  };
+
+  BOBChildArray.prototype.toString = function() {
+    return this.s();
+  };
+
+  BOBChildArray.prototype.s = function() {
+    var bob, html_string, _i, _len, _ref;
+    if (this.parent) {
+      return this.parent.s();
+    } else {
+      html_string = "";
+      _ref = this.bobs;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        bob = _ref[_i];
+        bob.parent = false;
+        html_string += bob.s();
+      }
+      return html_string;
+    }
+  };
+
+  BOBChildArray.prototype["do"] = function(data) {
+    return this.d(data);
+  };
+
+  BOBChildArray.prototype.d = function(data) {
+    var bob, _i, _len, _ref;
+    _ref = this.bobs;
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      bob = _ref[_i];
+      BOB._data = this.dataset[i];
+      if (this.bobs[i]) {
+        this.bobs[i] = this.bobs[i].d(data);
+      } else {
+        this.bobs.push(this.parent.d(data));
+      }
+    }
+    return this;
+  };
+
+  BOBChildArray.prototype.up = function() {
+    return this.u();
+  };
+
+  BOBChildArray.prototype.u = function() {
+    var i, _i, _ref;
+    if (!this.bobs[0]) {
+      BOB._data = null;
+      return this.parent;
+    }
+    for (i = _i = 0, _ref = this.bobs.length; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
+      this.bobs[i] = this.bobs[i].u();
+    }
+    if (this.bobs[0] === this.parent) {
+      BOB._data = null;
+      return this.parent;
+    } else {
+      return this;
+    }
+  };
+
+  return BOBChildArray;
+
+})();
+});
+
+;
+//# sourceMappingURL=BOB.js.map
